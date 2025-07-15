1b9d3e8e186b8958a9864e25ef0e6aedd7ee8e7b
Ritik Jangir <jangir.ritik06@gmail.com>
2025-07-15 23:08:01 +0530

    feat: codebase - integrate payload to the next app and update admin

---
+++ src/app/(payload)/admin/importMap.js
@@ -0,0 +1,5 @@
+import { default as default_00fe9c7cbc7f9c7508ed30aa9923b95d } from '../../../components/atoms/CustomIcon/CustomIcon'
+
+export const importMap = {
+  "/atoms/CustomIcon/CustomIcon#default": default_00fe9c7cbc7f9c7508ed30aa9923b95d
+}
