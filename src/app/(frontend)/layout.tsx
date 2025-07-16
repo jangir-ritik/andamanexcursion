bb354707bfc007857e4dccf4460536ca805496fa
Ritik Jangir <jangir.ritik06@gmail.com>
2025-07-16 18:05:58 +0530

    WIP: tanstack query - custom hooks, api client and implement provider pattern

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -11,6 +11,7 @@ import { TopLoadingBarProvider } from "@/components/layout/TopLoadingBarProvider
 import { Footer, Header } from "@/components/organisms";
 import { Column, Container } from "@/components/layout";
 import { PageBackgroundProvider } from "@/components/atoms/PageBackgroundProvider/PageBackgroundProvider";
+import { QueryProvider } from "@/providers/query-provider";
 
 export const metadata: Metadata = {
   title: "Andaman Excursion | Explore the Andaman Islands",
@@ -80,15 +81,17 @@ export default function RootLayout({
         <TopLoadingBarProvider>
           <BookingProviders>
             <PackageProvider>
-              <Header />
-              <PageBackgroundProvider>
-                <Container>
-                  <Column gap="var(--space-section)" fullWidth>
-                    {children}
-                  </Column>
-                </Container>
-              </PageBackgroundProvider>
-              <Footer />
+              <QueryProvider>
+                <Header />
+                <PageBackgroundProvider>
+                  <Container>
+                    <Column gap="var(--space-section)" fullWidth>
+                      {children}
+                    </Column>
+                  </Container>
+                </PageBackgroundProvider>
+                <Footer />
+              </QueryProvider>
             </PackageProvider>
           </BookingProviders>
         </TopLoadingBarProvider>
