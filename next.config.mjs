a8c486b5e0c9679d0aef370d7bebdcc450cc5598
Ritik Jangir <jangir.ritik06@gmail.com>
2025-08-29 02:22:00 +0530

    experimental - next confit - disable image transformations to see how it affects

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ import { withPayload } from "@payloadcms/next/withPayload";
 /** @type {import('next').NextConfig} */
 const nextConfig = withPayload({
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
