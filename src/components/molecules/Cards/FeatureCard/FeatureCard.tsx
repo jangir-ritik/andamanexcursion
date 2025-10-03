e3b89a2089f46f91e8aebac9ed56baf773871481
Ritik Jangir <jangir.ritik06@gmail.com>
2025-10-03 16:56:36 +0530

    fix: featureCard - update styles

--- src/components/molecules/Cards/FeatureCard/FeatureCard.tsx
+++ src/components/molecules/Cards/FeatureCard/FeatureCard.tsx
@@ -3,14 +3,18 @@ import React, { memo } from "react";
 import styles from "./FeatureCard.module.css";
 import { FeatureCardProps } from "./FeatureCard.types";
 import { cn } from "@/utils/cn";
-import { IconContainer } from "@/components/atoms/IconContainer/IconContainer";
+import MediaContainer from "@/components/atoms/MediaContainer/MediaContainer";
 
 export const FeatureCard = memo<FeatureCardProps>(
   ({ title, description, icon, className, ...props }) => {
     return (
       <article {...props} className={cn(styles.featureCard, className)}>
         <div className={styles.iconContainer} aria-hidden="true">
-          <IconContainer src={icon.url || ""} alt={icon.alt || ""} size={32} />
+          <MediaContainer
+            className={styles.icon}
+            src={icon.url || ""}
+            alt={icon.alt || ""}
+          />
         </div>
         <div className={styles.content}>
           <h3 className={styles.title}>{title}</h3>
